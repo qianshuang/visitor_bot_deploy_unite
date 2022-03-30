b13eb9c598dab2231ad0dc598927ccc5a32e7eb8
qianshuang <2009qianshuang@163.com>
2022-03-31 00:32:18 +0800

    update

--- test_concurrency.py
+++ test_concurrency.py
@@ -4,6 +4,8 @@ import requests
 import json
 from common import *
 
+import threading
+
 post_data = json.dumps({
     "bot_name": "banking demo4",
     "query": "cus",
@@ -11,8 +13,14 @@ post_data = json.dumps({
     "cn_enable": True
 })
 
-for i in range(20):
+
+def doWaiting():
     start = datetime.datetime.now()
     r = requests.post("http://127.0.0.1:8088/search", data=post_data)
     print(r.text)
     print(time_cost(start))
+
+
+for i in range(500):
+    t = threading.Thread(target=doWaiting)
+    t.start()
